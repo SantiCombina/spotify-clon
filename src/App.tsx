5272a31b331afbbe830b25e0c4056d16386dd8d0
SantiCombina <santicombina@gmail.com>
2024-10-07 19:29:13 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="min-h-[100dvh] bg-neutral-950 text-gray-300 flex flex-col justify-center items-center">
+            Hola Mundo!
+        </div>
+    );
+}
+
+export default App;
