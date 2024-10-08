17905746c3b393b898eca928ef0213bf55195be6
SantiCombina <santicombina@gmail.com>
2024-10-07 23:41:38 -0300

    Add Home component and update routing configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import {Route, Routes} from "react-router-dom";
+
+import {Home} from "./views/home";
+
 function App() {
     return (
         <div className="min-h-[100dvh] bg-neutral-950 text-gray-300 flex flex-col justify-center items-center">
-            Hola Mundo!
+            <Routes>
+                <Route element={<Home />} path="/" />
+            </Routes>
         </div>
     );
 }
